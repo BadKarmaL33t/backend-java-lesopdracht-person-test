4e7f854490472a7b740463fa278cba3a4b340f4e
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-06-20 16:21:49 +0200

    setName methode unit testen

--- src/test/java/nl/novi/PersonTest.java
+++ src/test/java/nl/novi/PersonTest.java
@@ -1,4 +1,22 @@
 package nl.novi;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PersonTest {
+
+    @Test
+    public void testSetName() {
+        //arrange:
+        Person person = new Person();
+        String expectedName = "John";
+
+        //act:
+        person.setName(expectedName);
+        String actualName = person.getName();
+
+        //assert:
+        assertEquals(expectedName, actualName);
+    }
 }
